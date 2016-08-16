0491b34dcbde2ec4fd1848fbaadd06df49da3952
hadesyi <hadesleeq@gmail.com>
2016-08-16 16:51:53 +0900

    Initial commit

---
+++ src/main/java/com/hadeslee/sample/Comma.java
@@ -0,0 +1,27 @@
+package com.hadeslee.sample;
+
+import java.text.DecimalFormat;
+
+/**
+ * Project: JavaLibrary
+ * FileName: Comma
+ * Date: 2016-08-16
+ * Time: 오후 2:16
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class Comma {
+    private String comma;
+    final private int money = 184551000;
+
+    public String Comma() {
+        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+        return comma = decimalFormat.format(money);
+    }
+
+    public int NonComma() {
+        return money;
+    }
+
+}
---
+++ src/test/java/com/hadeslee/sample/CommaTest.java
@@ -0,0 +1,25 @@
+package com.hadeslee.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Project: JavaLibrary
+ * FileName: CommaTest
+ * Date: 2016-08-16
+ * Time: 오후 2:21
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class CommaTest {
+    @Test
+    public void testComma() throws Exception {
+        Comma comma = new Comma();
+        assertEquals(184551000, comma.NonComma());
+        assertEquals("184,551,000", comma.Comma());
+    }
+
+
+}
